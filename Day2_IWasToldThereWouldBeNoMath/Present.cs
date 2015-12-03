7f56fee0038a666fba48c36b85b3793c7b35212f
Jason Moore <moorej@fias.wa.edu.au>
2015-12-03 13:44:56 +0800

    Day 2 Part 2
    Calculate length of ribbon required for each present.

---
+++ Day2_IWasToldThereWouldBeNoMath/Present.cs
@@ -0,0 +1,60 @@
+﻿#region Information
+
+// AdventOfCode: Day2_IWasToldThereWouldBeNoMath
+// Created: 2015-12-03
+// Modified: 2015-12-03 1:44 PM
+// Last modified by: MOORE Jason (jasonmo)
+#endregion
+
+#region Using Directives
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace Day2_IWasToldThereWouldBeNoMath
+{
+    internal class Present
+    {
+        #region Constructors
+        public Present() : this(0, 0, 0) {}
+
+        public Present(int length, int width, int height)
+        {
+            Height = height;
+            Length = length;
+            Width = width;
+        }
+        #endregion
+
+        #region Properties
+        public int Height { get; set; }
+        public int Length { get; set; }
+        public int Volume => Length * Width * Height;
+        public int Width { get; set; }
+        #endregion
+
+        /// <summary>
+        ///     Return the perimeter of the present, given the measurements of both sides.
+        /// </summary>
+        /// <param name="side1">Side A</param>
+        /// <param name="side2">Side B</param>
+        /// <returns></returns>
+        private static int Perimeter(int side1, int side2) => 2 * (side1 + side2);
+
+        /// <summary>
+        ///     Return the smallest perimeter of the present.
+        /// </summary>
+        /// <returns></returns>
+        public int SmallestPerimeter()
+        {
+            List<int> perimeters = new List<int>
+                                   {
+                                       Perimeter(Height, Length),
+                                       Perimeter(Height, Width),
+                                       Perimeter(Length, Width)
+                                   };
+            return perimeters.Min();
+        }
+    }
+}
--- Day2_IWasToldThereWouldBeNoMath/Program.cs
+++ Day2_IWasToldThereWouldBeNoMath/Program.cs
@@ -2,7 +2,7 @@
 
 // AdventOfCode: Day2_IWasToldThereWouldBeNoMath
 // Created: 2015-12-02
-// Modified: 2015-12-02 1:43 PM
+// Modified: 2015-12-03 1:43 PM
 // Last modified by: MOORE Jason (jasonmo)
 #endregion
 
@@ -23,26 +23,77 @@ namespace Day2_IWasToldThereWouldBeNoMath
             List<string> presentDimensionsList = File.ReadAllText("input.txt")
                                                      .Split(Environment.NewLine.ToCharArray(),
                                                             StringSplitOptions.RemoveEmptyEntries).ToList();
-            int totalArea = presentDimensionsList.Sum(s => GetPaperRequirements(s));
+            List<Present> presents = GetPresents(presentDimensionsList);
+            int totalArea = presents.Sum(s => GetPaperRequirements(s));
+            int totalRibbon = presents.Sum(p => GetRibbonRequirements(p));
             Console.WriteLine($"\r\n\r\nGrand Total of wrapping paper required: {totalArea} sq ft.");
+            Console.WriteLine($"Total ribbon required: {totalRibbon} ft.");
             Console.WriteLine("Press any key");
             Console.ReadKey();
         }
 
-        private static int GetPaperRequirements(string s)
+        /// <summary>
+        ///     Each present requires the smallest perimeter of one face, plus the cubic volume of the present.
+        /// </summary>
+        /// <param name="present"></param>
+        /// <returns></returns>
+        private static int GetRibbonRequirements(Present present)
         {
-            string[] dimensions = s.Split('x').ToArray();
-            int l = Convert.ToInt32(dimensions[0]);
-            int w = Convert.ToInt32(dimensions[1]);
-            int h = Convert.ToInt32(dimensions[2]);
-            List<int> sides = new List<int> {l * w, w * h, l * h};
-            int smallestSide = sides.Min();
-            int surfaceArea = 2 * sides.Sum();
-            int total = smallestSide + surfaceArea;
+            int ribbonLength = present.SmallestPerimeter();
+            int bowLength = present.Volume;
+            int totalRibbon = ribbonLength + bowLength;
             Console.WriteLine(
-                $"Present is {l} long, {w} wide, {h} high. Surface area: {surfaceArea} with {smallestSide} slack.");
-            Console.WriteLine($"Total area of {total}.");
+                $"Present {present.Length}L x {present.Width}W x {present.Height}H requires {ribbonLength} ft ribbon + {bowLength} bow, total {totalRibbon}.");
+            return totalRibbon;
+        }
+
+        /// <summary>
+        ///     Generate a list of Presents given a string in the format lxwxh
+        /// </summary>
+        /// <param name="presentDimensionsList"></param>
+        /// <returns></returns>
+        private static List<Present> GetPresents(IEnumerable<string> presentDimensionsList)
+        {
+            return
+                presentDimensionsList.Select(s => s.Split('x').ToArray()).Select(dimensions => new Present
+                                                                                               {
+                                                                                                   Length =
+                                                                                                       Convert.ToInt32(
+                                                                                                           dimensions[0]),
+                                                                                                   Width =
+                                                                                                       Convert.ToInt32(
+                                                                                                           dimensions[1]),
+                                                                                                   Height =
+                                                                                                       Convert.ToInt32(
+                                                                                                           dimensions[2])
+                                                                                               }).ToList();
+        }
+
+        /// <summary>
+        ///     Get paper required for present.
+        /// </summary>
+        /// <param name="p">Present to wrap</param>
+        /// <returns></returns>
+        private static int GetPaperRequirements(Present p)
+        {
+            List<int> areas = GetSideAreas(p);
+            int smallestArea = areas.Min();
+            int surfaceArea = 2 * areas.Sum();
+            int total = smallestArea + surfaceArea;
+            Console.WriteLine(
+                $"Surface area: {surfaceArea} with {smallestArea} slack. Total area of {total}.");
             return total;
+        }
+
+        /// <summary>
+        ///     Get area of sides.
+        /// </summary>
+        /// <param name="p">Present to measure.</param>
+        /// <returns></returns>
+        private static List<int> GetSideAreas(Present p)
+        {
+            Console.Write($"Present is {p.Length} long, {p.Width} wide, {p.Height} high. ");
+            return new List<int> {p.Length * p.Width, p.Width * p.Height, p.Length * p.Height};
         }
     }
 }
