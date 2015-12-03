d9c257010cc681d6517f9998473bb08070d30b10
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-04 07:05:53 +0800

    Day 3
    Start Day 3. Create House object with XY coordinates, and number of
    presents delivered.

---
+++ Day3_PerfectlySphericalHousesInAVacuum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibHouse;
+
+namespace Day3_PerfectlySphericalHousesInAVacuum
+{
+    class Program
+    {
+        private static List<House> _houses = new List<House>();
+
+        static void Main(string[] args)
+        {
+            string directions = ">";
+            
+        }
+
+        static int Move(House house, char direction)
+        {
+            int x = house.X;
+            int y = house.Y;
+
+            switch (direction)
+            {
+                case '^':
+                    return ++y;
+                case 'v':
+                    return --y;
+                case '>':
+                    return ++x;
+                case '<':
+                default:
+                    return --x;
+            }
+        }
+    }
+}
---
+++ LibHouse/House.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibHouse
+{
+    public class House
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Presents { get; set; }
+
+        public House() : this(0, 0) { }
+        public House(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
