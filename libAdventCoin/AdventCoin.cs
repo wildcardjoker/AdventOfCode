696856690f69492d001ed61ae24eea86bbbe9a7b
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-04 23:17:11 +0800

    Add MD5 hash algorithm
    Add MD5 hash algorithm from MSDN blog.

--- libAdventCoin/AdventCoin.cs
+++ libAdventCoin/AdventCoin.cs
@@ -2,12 +2,13 @@
 
 // AdventOfCode: libAdventCoin
 // Created: 2015-12-04
-// Modified: 2015-12-04 9:46 PM
+// Modified: 2015-12-04 11:16 PM
 // Last modified by: Jason Moore (Jason)
 #endregion
 
 #region Using Directives
 using System.Security.Cryptography;
+using System.Text;
 
 #endregion
 
@@ -17,17 +18,41 @@ namespace libAdventCoin
     {
         #region Constructors
         public AdventCoin() : this(null) {}
+        public AdventCoin(string key) : this(key, null) {}
 
-        public AdventCoin(string key)
+        public AdventCoin(string key, int? val)
         {
             Key = key;
+            Value = val;
         }
         #endregion
 
         #region Properties
         public string Key { get; set; }
-        public string Md5Hash => MD5.Create($"{Key}{Value}").ToString();
-        public int Value { get; set; }
+        public string Md5Hash => CalculateMD5Hash($"{Key}{Value}");
+        public int? Value { get; set; }
         #endregion
+
+        /// <summary>
+        ///     Calculate MD5H hash
+        /// </summary>
+        /// <param name="input">String to Hahs</param>
+        /// <returns>MD5 hash in Hex string</returns>
+        /// <remarks>http://blogs.msdn.com/b/csharpfaq/archive/2006/10/09/how-do-i-calculate-a-md5-hash-from-a-string_3f00_.aspx</remarks>
+        private string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            foreach (byte t in hash)
+            {
+                sb.Append(t.ToString("X2"));
+            }
+            return sb.ToString();
+        }
     }
 }
