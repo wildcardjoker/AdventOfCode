64bb5b1b06966d4e06e2b0268ab572ffccebb113
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-04 23:17:46 +0800

    Day 4 Part 1
    Calculate lowest number generating MD5 hash starting with 5 zeros.

---
+++ Day4_IdealStockingStuffer/Program.cs
@@ -0,0 +1,41 @@
+﻿#region Information
+
+// AdventOfCode: Day4_IdealStockingStuffer
+// Created: 2015-12-04
+// Modified: 2015-12-04 11:10 PM
+// Last modified by: Jason Moore (Jason)
+#endregion
+
+#region Using Directives
+using System;
+using libAdventCoin;
+
+#endregion
+
+namespace Day4_IdealStockingStuffer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            AdventCoin coin = new AdventCoin("ckczppom", 0);
+            bool mined = false;
+            while (!mined)
+            {
+                if (coin.Value % 1000 == 0)
+                {
+                    Console.WriteLine($"Processed {coin.Value} values...");
+                }
+                if (coin.Md5Hash.StartsWith("00000"))
+                {
+                    mined = true;
+                    break;
+                }
+                coin.Value++;
+            }
+            Console.WriteLine($"Coin mined with value {coin.Value}. Hash: {coin.Md5Hash}");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
