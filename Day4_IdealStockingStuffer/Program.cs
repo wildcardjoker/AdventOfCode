56a536936bffa2c1b1507b33dd659d9bcfcc50b0
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-04 23:30:39 +0800

    Day 4 part 2
    Modify application to get values for the first hash starting with 00000
    and 000000.

--- Day4_IdealStockingStuffer/Program.cs
+++ Day4_IdealStockingStuffer/Program.cs
@@ -2,7 +2,7 @@
 
 // AdventOfCode: Day4_IdealStockingStuffer
 // Created: 2015-12-04
-// Modified: 2015-12-04 11:10 PM
+// Modified: 2015-12-04 11:27 PM
 // Last modified by: Jason Moore (Jason)
 #endregion
 
@@ -19,23 +19,37 @@ namespace Day4_IdealStockingStuffer
         private static void Main(string[] args)
         {
             AdventCoin coin = new AdventCoin("ckczppom", 0);
+
+            // Part 1
+            string part1 = GetLowestNumber(coin, "00000");
+
+            // Part 2
+            coin.Value = 0;
+            string part2 = GetLowestNumber(coin, "000000");
+
+            Console.WriteLine(part1);
+            Console.WriteLine(part2);
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static string GetLowestNumber(AdventCoin coin, string s)
+        {
             bool mined = false;
             while (!mined)
             {
-                if (coin.Value % 1000 == 0)
+                if (coin.Value % 100000 == 0)
                 {
                     Console.WriteLine($"Processed {coin.Value} values...");
                 }
-                if (coin.Md5Hash.StartsWith("00000"))
+                if (coin.Match(s))
                 {
                     mined = true;
                     break;
                 }
                 coin.Value++;
             }
-            Console.WriteLine($"Coin mined with value {coin.Value}. Hash: {coin.Md5Hash}");
-            Console.Write("Press any key...");
-            Console.ReadKey();
+            return $"Coin starting with {s} mined with value {coin.Value}. Hash: {coin.Md5Hash}";
         }
     }
 }
