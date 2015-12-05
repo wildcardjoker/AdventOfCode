0a7108e9590a9990391e58f9de26669a83a1255f
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-05 21:33:21 +0800

    Start Part 2
    Modify Santa object for Part 2.

---
+++ libSanta/Day5Part2.cs
@@ -0,0 +1,53 @@
+﻿#region Information
+
+// AdventOfCode: libSanta
+// Created: 2015-12-05
+// Modified: 2015-12-05 9:21 PM
+// Last modified by: Jason Moore (Jason)
+#endregion
+
+#region Using Directives
+#endregion
+
+#region Using Directives
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+#endregion
+
+namespace libSanta
+{
+    public partial class Santa
+    {
+        public bool IsNiceString2(string s)
+        {
+            return MatchTwoLetters(s) && MatchRepeatingCharacter(s);
+        }
+
+        /// <summary>
+        ///     Does string contain at least one letter that appears twice in a row?
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool MatchTwoLetters(string s)
+        {
+            Regex regex = new Regex(@"(\w\w)");
+            var matches = regex.Matches(s);
+            List<string> results = (from Match match in matches select match.Value).ToList();
+            var duplicates = results.GroupBy(m => m).SelectMany(grp => grp.Skip(1));
+            return duplicates.Any();
+        }
+
+        /// <summary>
+        ///     Match a string character between two matching characters.
+        /// </summary>
+        /// <param name="s">String to prcoess</param>
+        /// <returns>True if match found.</returns>
+        public bool MatchRepeatingCharacter(string s)
+        {
+            Regex regex = new Regex(@"(\w).\1");
+            return regex.Match(s).Success;
+        }
+    }
+}
--- libSanta/Santa.cs
+++ libSanta/Santa.cs
@@ -1,68 +1,17 @@
-﻿#region Information
-
-// AdventOfCode: libSanta
-// Created: 2015-12-05
-// Modified: 2015-12-05 2:30 PM
-// Last modified by: Jason Moore (Jason)
-#endregion
-
-#region Using Directives
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-
-#endregion
+using System.Text;
+using System.Threading.Tasks;
 
 namespace libSanta
 {
-    public class Santa
+    public partial class Santa
     {
         #region  Fields
         private static readonly char[] Vowels = "aeiou".ToCharArray();
-        private static readonly string[] BadStrings = {"ab", "cd", "pq", "xy"};
+        private static readonly string[] BadStrings = { "ab", "cd", "pq", "xy" };
         #endregion
 
-        public bool IsNiceString(string s)
-        {
-            return ContainsDoubleLetter(s) && ContainsVowels(s, 3) && !ContainsBadStrings(s);
-        }
-
-        /// <summary>
-        ///     Does string contain specified number of vowels?
-        /// </summary>
-        /// <param name="s">string to process</param>
-        /// <param name="num"></param>
-        /// <returns></returns>
-        private static bool ContainsVowels(string s, int num)
-        {
-            return s.Count(c => Vowels.Contains(c)) >= num;
-        }
-
-        /// <summary>
-        ///     Does string contain at least one letter that appears twice in a row?
-        /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
-        private static bool ContainsDoubleLetter(string s)
-        {
-            int j = 0;
-            for (int i = 0; i < s.Length - 1; i++)
-            {
-                j++;
-                if (s[i].Equals(s[j]))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        /// <summary>
-        ///     Does string contain any strings on the Naughty List?
-        /// </summary>
-        /// <param name="s">string to process</param>
-        /// <returns></returns>
-        private static bool ContainsBadStrings(string s)
-        {
-            return BadStrings.Any(s.Contains);
-        }
     }
 }
