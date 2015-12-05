de0703bdd0610a6364fa90355eea5cf6c77ee8fd
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-05 14:36:16 +0800

    Split to discrete methods
    Split rules into separate methods.

--- libSanta/Santa.cs
+++ libSanta/Santa.cs
@@ -2,7 +2,7 @@
 
 // AdventOfCode: libSanta
 // Created: 2015-12-05
-// Modified: 2015-12-05 2:06 PM
+// Modified: 2015-12-05 2:30 PM
 // Last modified by: Jason Moore (Jason)
 #endregion
 
@@ -16,25 +16,53 @@ namespace libSanta
     public class Santa
     {
         #region  Fields
-        private readonly string[] _badStrings = {"ab", "cd", "pq", "xy"};
-        private readonly char[] _vowels = "aeiuo".ToCharArray();
+        private static readonly char[] Vowels = "aeiou".ToCharArray();
+        private static readonly string[] BadStrings = {"ab", "cd", "pq", "xy"};
         #endregion
 
         public bool IsNiceString(string s)
         {
-            bool doubleLetter = false;
+            return ContainsDoubleLetter(s) && ContainsVowels(s, 3) && !ContainsBadStrings(s);
+        }
+
+        /// <summary>
+        ///     Does string contain specified number of vowels?
+        /// </summary>
+        /// <param name="s">string to process</param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private static bool ContainsVowels(string s, int num)
+        {
+            return s.Count(c => Vowels.Contains(c)) >= num;
+        }
+
+        /// <summary>
+        ///     Does string contain at least one letter that appears twice in a row?
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private static bool ContainsDoubleLetter(string s)
+        {
             int j = 0;
             for (int i = 0; i < s.Length - 1; i++)
             {
                 j++;
                 if (s[i].Equals(s[j]))
                 {
-                    doubleLetter = true;
-                    break;
+                    return true;
                 }
             }
-            return doubleLetter && s.Count(c => _vowels.Contains(c)) >= 3 &&
-                   !_badStrings.Any(x => _badStrings.Contains(x));
+            return false;
+        }
+
+        /// <summary>
+        ///     Does string contain any strings on the Naughty List?
+        /// </summary>
+        /// <param name="s">string to process</param>
+        /// <returns></returns>
+        private static bool ContainsBadStrings(string s)
+        {
+            return BadStrings.Any(s.Contains);
         }
     }
 }
