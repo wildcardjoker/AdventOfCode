ace4d4ad4988052286fe9a9f088936401b7b8f65
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-06 22:34:47 +0800

    TurnOnRange() method.
    Add method to turn on a range of lights.

---
+++ libChristmasLightsGrid.ViewModel/PerformInstructions.cs
@@ -0,0 +1,34 @@
+﻿#region Information
+
+// AdventOfCode: libChristmasLightsGrid.ViewModel
+// Created: 2015-12-06
+// Modified: 2015-12-06 10:31 PM
+// Last modified by: Jason Moore (Jason)
+#endregion
+
+#region Using Directives
+using System.Collections.Generic;
+using System.Linq;
+using libChristmasLight;
+
+#endregion
+
+namespace libChristmasLightsGrid.ViewModel
+{
+    public partial class ChristmasLightsGrid
+    {
+        private void TurnOnRange()
+        {
+            IEnumerable<ChristmasLight> range =
+                Lights.Where(
+                    light =>
+                    light.PosX <= RangeToModify.EndX && light.PosX >= RangeToModify.StartX &&
+                    light.PosY >= RangeToModify.StartY && light.PosY <= RangeToModify.EndY);
+
+            foreach (ChristmasLight light in range)
+            {
+                light.TurnOn();
+            }
+        }
+    }
+}
