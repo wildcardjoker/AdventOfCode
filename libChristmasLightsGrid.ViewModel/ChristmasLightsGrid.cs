0e390b6c47910593501dafdf49d8c0ba2beb09b2
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-06 19:04:17 +0800

    Move properties to separate file.
    Move property declarations to Properties.cs

--- libChristmasLightsGrid.ViewModel/ChristmasLightsGrid.cs
+++ libChristmasLightsGrid.ViewModel/ChristmasLightsGrid.cs
@@ -2,7 +2,7 @@
 
 // AdventOfCode: libChristmasLightsGrid.ViewModel
 // Created: 2015-12-06
-// Modified: 2015-12-06 5:57 PM
+// Modified: 2015-12-06 6:38 PM
 // Last modified by: Jason Moore (Jason)
 #endregion
 
@@ -17,65 +17,12 @@ using libChristmasLightsGrid.ViewModel.Annotations;
 
 namespace libChristmasLightsGrid.ViewModel
 {
-    public class ChristmasLightsGrid : INotifyPropertyChanged
+    public partial class ChristmasLightsGrid : INotifyPropertyChanged
     {
         #region  Fields
         private int _columns;
         private List<ChristmasLight> _lights;
         private int _rows;
-        #endregion
-
-        #region Properties
-        /// <summary>
-        ///     Number of Columns in Grid
-        /// </summary>
-        public int Columns
-        {
-            get { return _columns; }
-            set
-            {
-                if (value == _columns)
-                {
-                    return;
-                }
-                _columns = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        ///     List representing a Grid of Christmas Lights
-        /// </summary>
-        public List<ChristmasLight> Lights
-        {
-            get { return _lights; }
-            set
-            {
-                if (Equals(value, _lights))
-                {
-                    return;
-                }
-                _lights = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        ///     Number of Rows in Grid
-        /// </summary>
-        public int Rows
-        {
-            get { return _rows; }
-            set
-            {
-                if (value == _rows)
-                {
-                    return;
-                }
-                _rows = value;
-                OnPropertyChanged();
-            }
-        }
         #endregion
 
         #region INotifyPropertyChanged Members
---
+++ libChristmasLightsGrid.ViewModel/Properties.cs
@@ -0,0 +1,86 @@
+﻿#region Information
+
+// AdventOfCode: libChristmasLightsGrid.ViewModel
+// Created: 2015-12-06
+// Modified: 2015-12-06 6:50 PM
+// Last modified by: Jason Moore (Jason)
+#endregion
+
+#region Using Directives
+using System;
+using System.Collections.Generic;
+using libChristmasLight;
+
+#endregion
+
+namespace libChristmasLightsGrid.ViewModel
+{
+    public partial class ChristmasLightsGrid
+    {
+        #region  Fields
+        public const string ArgumentOutOfRangeMessage =
+            "Value is out of range. Acceptable values are any positive integer.";
+        #endregion
+
+        #region Properties
+        /// <summary>
+        ///     Number of Columns in Grid
+        /// </summary>
+        public int Columns
+        {
+            get { return _columns; }
+            set
+            {
+                if (value == _columns)
+                {
+                    return;
+                }
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, ArgumentOutOfRangeMessage);
+                }
+                _columns = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     List representing a Grid of Christmas Lights
+        /// </summary>
+        public List<ChristmasLight> Lights
+        {
+            get { return _lights; }
+            set
+            {
+                if (Equals(value, _lights))
+                {
+                    return;
+                }
+                _lights = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     Number of Rows in Grid
+        /// </summary>
+        public int Rows
+        {
+            get { return _rows; }
+            set
+            {
+                if (value == _rows)
+                {
+                    return;
+                }
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, ArgumentOutOfRangeMessage);
+                }
+                _rows = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
