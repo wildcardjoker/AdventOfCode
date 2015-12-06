1ac9e2998dfceedd5057706e93da31d5b1e4c5b7
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-06 19:04:39 +0800

    Add Commands.
    Add methods and commands.

---
+++ libChristmasLightsGrid.ViewModel/Commands.cs
@@ -0,0 +1,36 @@
+﻿#region Information
+
+// AdventOfCode: libChristmasLightsGrid.ViewModel
+// Created: 2015-12-06
+// Modified: 2015-12-06 6:37 PM
+// Last modified by: Jason Moore (Jason)
+#endregion
+
+#region Using Directives
+using System.Windows.Input;
+using libRelayCommand;
+
+#endregion
+
+namespace libChristmasLightsGrid.ViewModel
+{
+    public partial class ChristmasLightsGrid
+    {
+        #region  Fields
+        private ICommand _generateGridCommand;
+        #endregion
+
+        #region Properties
+        public ICommand GenerateGridCommand
+        {
+            get
+            {
+                return
+                    _generateGridCommand ?? (_generateGridCommand =
+                                             new RelayCommand(param => CreateGrid(),
+                                                              canExecute => Rows > 0 && Columns > 0));
+            }
+        }
+        #endregion
+    }
+}
---
+++ libChristmasLightsGrid.ViewModel/Methods.cs
@@ -0,0 +1,32 @@
+﻿#region Information
+
+// AdventOfCode: libChristmasLightsGrid.ViewModel
+// Created: 2015-12-06
+// Modified: 2015-12-06 6:38 PM
+// Last modified by: Jason Moore (Jason)
+#endregion
+
+#region Using Directives
+using libChristmasLight;
+
+#endregion
+
+namespace libChristmasLightsGrid.ViewModel
+{
+    public partial class ChristmasLightsGrid
+    {
+        /// <summary>
+        ///     Create a grid Columns wide x Rows high
+        /// </summary>
+        private void CreateGrid()
+        {
+            for (int i = 0; i < Columns; i++)
+            {
+                for (int j = 0; j < Rows; j++)
+                {
+                    Lights.Add(new ChristmasLight(i, j));
+                }
+            }
+        }
+    }
+}
