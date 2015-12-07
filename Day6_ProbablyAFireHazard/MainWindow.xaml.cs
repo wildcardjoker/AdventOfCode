69e3f568694217181217b6349e12e12bed5c1777
Jason Moore <moorej@fias.wa.edu.au>
2015-12-07 13:08:58 +0800

    GUI app for Day 6 part 1

---
+++ Day6_ProbablyAFireHazard/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿#region Information
+
+// AdventOfCode: Day6_ProbablyAFireHazard
+// Created: 2015-12-07
+// Modified: 2015-12-07 1:02 PM
+// Last modified by: MOORE Jason (jasonmo)
+#endregion
+
+#region Using Directives
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Day6_ProbablyAFireHazard.ValueConverters;
+using libChristmasLightsGrid.ViewModel;
+
+#endregion
+
+namespace Day6_ProbablyAFireHazard
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        private void GenerateGrid_Click(object sender, RoutedEventArgs e)
+        {
+            ChristmasLightsGrid dc = (ChristmasLightsGrid) DataContext;
+
+            dc.GenerateGridCommand.Execute(null);
+            for (int y = 0; y < dc.Rows; y++)
+            {
+                LightGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int x = 0; x < dc.Columns; x++)
+            {
+                LightGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int x = 0; x < dc.Columns; x++)
+            {
+                for (int y = 0; y < dc.Rows; y++)
+                {
+                    TextBlock cell = new TextBlock();
+                    cell.SetBinding(TextBlock.TextProperty,
+                                    new Binding
+                                    {
+                                        Path =
+                                            new PropertyPath(
+                                            $"Lights[{dc.Lights.FindIndex(l => l.PosX == x && l.PosY == y)}].Lit"),
+                                        Source = dc,
+                                        Converter = new BoolToStringConverter(),
+                                        UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                                        IsAsync = true
+                                    });
+                    Grid.SetColumn(cell, x);
+                    Grid.SetRow(cell, y);
+                    LightGrid.Children.Add(cell);
+                }
+            }
+        }
+    }
+}
---
+++ Day6_ProbablyAFireHazard/ValueConverters/BoolToStringConverter.cs
@@ -0,0 +1,52 @@
+﻿#region Information
+
+// AdventOfCode: Day6_ProbablyAFireHazard
+// Created: 2015-12-07
+// Modified: 2015-12-07 12:32 PM
+// Last modified by: MOORE Jason (jasonmo)
+#endregion
+
+#region Using Directives
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+#endregion
+
+namespace Day6_ProbablyAFireHazard.ValueConverters
+{
+    class BoolToStringConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+        /// <summary>
+        ///     Converts a value.
+        /// </summary>
+        /// <returns>
+        ///     A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool) value ? "1" : "0";
+        }
+
+        /// <summary>
+        ///     Converts a value.
+        /// </summary>
+        /// <returns>
+        ///     A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        /// <param name="value">The value that is produced by the binding target.</param>
+        /// <param name="targetType">The type to convert to.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
