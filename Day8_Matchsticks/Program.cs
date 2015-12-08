cac34b2063f88349b26e6e919b6e2b3ffaa9f2cb
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-08 22:20:25 +0800

    Day 8 Part 2
    Re-encode literal strings and calculate the number of additional
    characters after encoding.

--- Day8_Matchsticks/Program.cs
+++ Day8_Matchsticks/Program.cs
@@ -2,12 +2,14 @@
 
 // AdventOfCode: Day8_Matchsticks
 // Created: 2015-12-08
-// Modified: 2015-12-08 9:46 PM
+// Modified: 2015-12-08 10:17 PM
 // Last modified by: Jason Moore (Jason)
 #endregion
 
 #region Using Directives
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -20,10 +22,22 @@ namespace Day8_Matchsticks
         static void Main(string[] args)
         {
             string[] inputList = File.ReadAllLines("input.txt");
+            CalculateSanitisedInput(inputList);
+            CalculateEncodedInput(inputList);
+            Console.Write("Press any key.");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        ///     Day 8 Part 1 - Calculate number of literal string characters
+        /// </summary>
+        /// <param name="strings">Puzzle input</param>
+        private static void CalculateSanitisedInput(IEnumerable<string> strings)
+        {
             int totalCodeChars = 0;
             int totalStringChars = 0;
 
-            foreach (string s in inputList)
+            foreach (string s in strings)
             {
                 int codeChars = s.Length;
                 string sanitisedString = SanitiseString(s);
@@ -34,11 +48,38 @@ namespace Day8_Matchsticks
             }
             Console.WriteLine($"{totalCodeChars} total code characters.");
             Console.WriteLine($"{totalStringChars} total string characters.");
-            Console.WriteLine($"{totalCodeChars - totalStringChars} total characters.");
-            Console.Write("Press any key.");
-            Console.ReadKey();
+            string totalSanitisedChars = $"{totalCodeChars - totalStringChars} total characters.";
+            Console.WriteLine(totalSanitisedChars);
+            Debug.WriteLine(totalSanitisedChars);
         }
 
+        private static void CalculateEncodedInput(IEnumerable<string> strings)
+        {
+            int totalCodeChars = 0;
+            int totalStringChars = 0;
+
+            foreach (string s in strings)
+            {
+                int codeChars = s.Length;
+                string encodedString = EncodeString(s);
+                int stringChars = encodedString.Length;
+                Console.WriteLine(
+                    $"{s} ({codeChars} code) -> {encodedString} ({stringChars} characters).");
+                totalCodeChars += codeChars;
+                totalStringChars += stringChars;
+            }
+            Console.WriteLine($"{totalCodeChars} total code characters.");
+            Console.WriteLine($"{totalStringChars} total string characters.");
+            string totalSanitisedChars = $"{totalStringChars - totalCodeChars} encoded characters.";
+            Console.WriteLine(totalSanitisedChars);
+            Debug.WriteLine(totalSanitisedChars);
+        }
+
+        /// <summary>
+        ///     Decode string literals
+        /// </summary>
+        /// <param name="s">Puzzle input</param>
+        /// <returns></returns>
         private static string SanitiseString(string s)
         {
             // Remove beginning and end quotes.
@@ -49,6 +90,16 @@ namespace Day8_Matchsticks
 
             //Convert to new string.
             return sanitisedString;
+        }
+
+        /// <summary>
+        ///     Re-encode each literal string character
+        /// </summary>
+        /// <param name="s">Puzzle input</param>
+        /// <returns></returns>
+        private static string EncodeString(string s)
+        {
+            return $"\"{(s.Replace("\\", "\\\\").Replace("\"", "\\\""))}\"";
         }
     }
 }
