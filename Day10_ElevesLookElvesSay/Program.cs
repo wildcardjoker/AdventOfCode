b12c0c2d5a83ab73ab8c5444b0d904a708c8f7a8
wildcardjoker <the.pctechnician+github@gmail.com>
2015-12-10 22:21:43 +0800

    Day 10 Part 1

---
+++ Day10_ElevesLookElvesSay/Program.cs
@@ -0,0 +1,61 @@
+﻿#region Information
+
+// AdventOfCode: Day10_ElevesLookElvesSay
+// Created: 2015-12-10
+// Modified: 2015-12-10 10:20 PM
+// Last modified by: Jason Moore (Jason)
+#endregion
+
+#region Using Directives
+#endregion
+
+#region Using Directives
+using System;
+using System.Diagnostics;
+
+#endregion
+
+namespace Day10_ElevesLookElvesSay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string input = "1113222113";
+            string output = input;
+
+            Console.Write("Crunching numbers...");
+
+            for (int i = 0; i < 40; i++)
+            {
+                string s = string.Empty;
+                char[] chars = output.ToCharArray();
+                int j = 0;
+                int k = 1;
+                while (k < chars.Length)
+                {
+                    int count = 1;
+                    while (chars[j] == chars[k])
+                    {
+                        count++;
+                        j++;
+                        k++;
+                    }
+                    s += $"{count}{chars[j]}";
+                    j++;
+                    k++;
+                }
+                if (k == chars.Length)
+                {
+                    s += $"1{chars[j]}";
+                }
+                output = s;
+            }
+            string answer = $"Length: {output} ({output.Length} characters).";
+            Debug.WriteLine(answer);
+            Console.WriteLine(answer);
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
+        }
+    }
+}
