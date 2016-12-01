977174bae6c18ebeebd353c695ff13ea4561e3c8
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-01 20:11:52 +0800

    Add Day 12.

--- Day12_JSAbacusFramework.io/Program.cs
+++ Day12_JSAbacusFramework.io/Program.cs
@@ -2,7 +2,7 @@
 
 // AdventOfCode: Day12_JSAbacusFramework.io
 // Created: 2015-12-12
-// Modified: 2015-12-12 9:13 PM
+// Modified: 2015-12-12 9:53 PM
 // Last modified by: Jason Moore (Jason)
 #endregion
 
@@ -22,14 +22,28 @@ namespace Day12_JSAbacusFramework.io
         static void Main(string[] args)
         {
             string input = File.ReadAllText("input.txt");
-            Regex regex = new Regex("-?\\d+");
+            string part1Pattern = "-?\\d+";
+            string part2Pattern = @"({^\[.*red.*^\]})|(.)";
+
+            // Part 1
+            GetSum(input, part1Pattern);
+
+            // Part 2
+            Regex regex = new Regex(part2Pattern);
+            input = Regex.Replace(input, part2Pattern, "$2");
+            GetSum(input, part1Pattern);
+            Console.Write("Press any key.");
+            Console.ReadKey();
+        }
+
+        private static void GetSum(string input, string pattern)
+        {
+            Regex regex = new Regex(pattern);
             int i = regex.Matches(input).Cast<Match>().Sum(match => Convert.ToInt32(match.Value));
 
             string result = $"Sum Total of all values: {i}.";
             Console.WriteLine(result);
             Debug.WriteLine(result);
-            Console.Write("Press any key.");
-            Console.ReadKey();
         }
     }
 }
