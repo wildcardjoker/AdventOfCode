3ac52f7337da26e046f59b0475bd2b91f8974d7b
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-02 21:59:18 +0800

    Convert PadMax to constant.

--- Day2_BathroomSecurity/Program.cs
+++ Day2_BathroomSecurity/Program.cs
@@ -25,7 +25,7 @@ namespace Day2_BathroomSecurity
         private static readonly List<Button> KeyPad = new List<Button>();
         private static int _padX = 1;
         private static int _padY = 1;
-        private static readonly int padMax = 2;
+        private const int PadMax = 2;
         private static readonly StringBuilder Sb = new StringBuilder();
         #endregion
 
@@ -52,9 +52,9 @@ namespace Day2_BathroomSecurity
         private static void GenerateKeyPad()
         {
             var k = 1;
-            for (int i = padMax; i >= 0; i--)
+            for (int i = PadMax; i >= 0; i--)
             {
-                for (var j = 0; j <= padMax; j++)
+                for (var j = 0; j <= PadMax; j++)
                 {
                     KeyPad.Add(new Button(j, i, k));
                     k++;
@@ -95,8 +95,8 @@ namespace Day2_BathroomSecurity
                     Debug.WriteLine($"Unknown direction: {c}");
                     break;
             }
-            _padX = _padX < 0 ? 0 : _padX > padMax ? padMax : _padX;
-            _padY = _padY < 0 ? 0 : _padY > padMax ? padMax : _padY;
+            _padX = _padX < 0 ? 0 : _padX > PadMax ? PadMax : _padX;
+            _padY = _padY < 0 ? 0 : _padY > PadMax ? PadMax : _padY;
         }
     }
 }
