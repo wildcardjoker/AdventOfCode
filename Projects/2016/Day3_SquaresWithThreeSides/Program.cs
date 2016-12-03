f56f2cab52c56905a335f9e6d5ee67f505871978
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-03 21:41:24 +0800

    Add Day 3 - completed Part 1.

---
+++ Projects/2016/Day3_SquaresWithThreeSides/Program.cs
@@ -0,0 +1,53 @@
+﻿#region Information
+
+// AdventOfCode: Day3_SquaresWithThreeSides
+// Created: 2016-12-03
+// Modified: 2016-12-03 9:26 PM
+#endregion
+
+#region Using Directives
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+#endregion
+
+namespace Day3_SquaresWithThreeSides
+{
+    /// <summary>
+    ///     Day 3 - Squares with three sides
+    ///     Given a list of triangles (three sides of varying length),
+    ///     calculate the number of records which may be a triangle
+    ///     i.e. how many records have a set of numbers where the
+    ///     two smallest numbers are larger than the largest number.
+    /// </summary>
+    class Program
+    {
+        #region  Fields
+        private static List<string> _input;
+        private static readonly List<Triangle> Triangles = new List<Triangle>();
+        #endregion
+
+        static void Main(string[] args)
+        {
+            // Test cases
+            //_input = new List<string> {"5 10 25", "5 20 25", "10 20 25"};
+            _input = File.ReadAllLines("input.txt").ToList();
+
+            // Convert input to Triangle objects.
+            ProcessInput();
+            Console.WriteLine($"Number of possible triangles found: {Triangles.Count(x => x.IsValidTriangle)}.");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void ProcessInput()
+        {
+            foreach (string possibleTriangle in _input)
+            {
+                Triangles.Add(new Triangle(possibleTriangle));
+            }
+        }
+    }
+}
---
+++ Projects/2016/Day3_SquaresWithThreeSides/Triangle.cs
@@ -0,0 +1,72 @@
+﻿#region Information
+
+// AdventOfCode: Day3_SquaresWithThreeSides
+// Created: 2016-12-03
+// Modified: 2016-12-03 9:41 PM
+#endregion
+
+#region Using Directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace Day3_SquaresWithThreeSides
+{
+    /// <summary>
+    ///     Three lengths representing the sides of a triangle.
+    /// </summary>
+    public class Triangle
+    {
+        #region Constructors
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Triangle" /> class.
+        /// </summary>
+        /// <param name="lengths">The lengths.</param>
+        public Triangle(string lengths)
+        {
+            Sides =
+                lengths.Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                       .Select(x => Convert.ToInt32(x)).OrderBy(x => x)
+                       .ToList();
+            LongestSide = Sides.Last();
+            SumOfRemainingSides = Sides.OrderByDescending(x => x).Skip(1).Sum();
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        ///     Gets a value indicating whether this instance is a valid triangle.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this instance is valid triangle; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsValidTriangle => SumOfRemainingSides > LongestSide;
+
+        /// <summary>
+        ///     Gets the longest side.
+        /// </summary>
+        /// <value>
+        ///     The longest side.
+        /// </value>
+        private int LongestSide { get; }
+
+        /// <summary>
+        ///     Gets or sets the sides of the triangle.
+        /// </summary>
+        /// <value>
+        ///     The sides.
+        /// </value>
+        public List<int> Sides { get; set; }
+
+        /// <summary>
+        ///     Gets the sum of remaining sides.
+        /// </summary>
+        /// <value>
+        ///     The sum of remaining sides.
+        /// </value>
+        private int SumOfRemainingSides { get; }
+        #endregion
+    }
+}
