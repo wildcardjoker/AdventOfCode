1131f2caf11e5211bde8ac6ea576291e4e33940f
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-07 22:54:55 +0800

    Day 7 part 1
    Completed Day 7 part 1.

---
+++ Projects/2016/Day7_InternetProtocolVersion7/Ipv7Address.cs
@@ -0,0 +1,89 @@
+﻿#region Information
+
+// AdventOfCode: Day7_InternetProtocolVersion7
+// Created: 2016-12-07
+// Modified: 2016-12-07 10:50 PM
+#endregion
+
+#region Using Directives
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace Day7_InternetProtocolVersion7
+{
+    class Ipv7Address
+    {
+        #region Constructors
+        public Ipv7Address(string input)
+        {
+            Segments = new List<string>();
+            HyperNetSegments = new List<string>();
+            Input = input;
+
+            // There has to be an easier way to split a string on [ or ] but I couldn't figure out the regular expression.
+            // Let's do it the easy way.
+            List<string> segments = Input.Replace("[", "-").Replace("]", "-").Split('-').ToList();
+            for (var i = 0; i < segments.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Segments.Add(segments[i]);
+                }
+                else
+                {
+                    HyperNetSegments.Add(segments[i]);
+                }
+            }
+        }
+        #endregion
+
+        #region Properties
+        public List<string> HyperNetSegments { get; set; }
+
+        public int HyperNetSegmentsWithAbba => HyperNetSegments.Count(ContainsAbba);
+        public string Input { get; set; }
+        public List<string> Segments { get; set; }
+        public int SegmentsWithAbba => Segments.Count(ContainsAbba);
+        #endregion
+
+        /// <summary>
+        ///     Indicates whether the input string represents an IPv7 address that supports TLA according to Part 1.
+        /// </summary>
+        /// <returns></returns>
+        public bool SupportsTla()
+        {
+            // Can be condensed into an expression body, but this is easier to read.
+            bool containsAbbaSegmentsInHyperNetSegment = HyperNetSegments.Any(ContainsAbba);
+            bool containsAbbaSegments = Segments.Any(ContainsAbba);
+            return containsAbbaSegments && !containsAbbaSegmentsInHyperNetSegment;
+        }
+
+        /// <summary>
+        /// Determines whether the specified input contains a string in the pattern "abba".
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified input contains "abba" pattern; otherwise, <c>false</c>.
+        /// </returns>
+        public bool ContainsAbba(string input)
+        {
+            // Check for abba pattern.
+            char[] segment = input.ToCharArray();
+            for (var i = 0; i < segment.Length - 3; i++)
+            {
+                // Given 4 characters abcd, check that a==d and b==c
+                if (segment[i].Equals(segment[i + 3]) && segment[i + 1].Equals(segment[i + 2]))
+                {
+                    // abba pattern has been detected
+                    // Return false if a==b
+                    return !segment[i].Equals(segment[i + 1]);
+                }
+            }
+
+            // Failed to find abba pattern
+            return false;
+        }
+    }
+}
---
+++ Projects/2016/Day7_InternetProtocolVersion7/Program.cs
@@ -0,0 +1,55 @@
+﻿#region Information
+
+// AdventOfCode: Day7_InternetProtocolVersion7
+// Created: 2016-12-07
+// Modified: 2016-12-07 10:54 PM
+#endregion
+
+//#define TEST
+
+#region Using Directives
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+#endregion
+
+namespace Day7_InternetProtocolVersion7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if TEST
+            var addresses = new List<Ipv7Address>
+                            {
+                                // example input
+                                //new Ipv7Address("abba[mnop]qrst"),
+                                //new Ipv7Address("abcd[bddb]xyyx"),
+                                //new Ipv7Address("aaaa[qwer]tyui"),
+                                //new Ipv7Address("ioxxoj[asdfgh]zxcvbn")
+                            };
+
+#else
+            List<Ipv7Address> addresses =
+                File.ReadAllLines("input.txt").Select(x => new Ipv7Address(x)).ToList();
+#endif
+
+            // Get input with "abba" pattern.
+            Console.WriteLine($"Found {addresses.Count(x => x.SegmentsWithAbba > 0)} segments.");
+
+            // Get input with "abba" pattern inside [].
+            Console.WriteLine($"Found {addresses.Count(x => x.HyperNetSegmentsWithAbba > 0)} hypernet segments");
+
+            // Get input with "abba" pattern both inside and outside [].
+            Console.WriteLine(
+                $"Found {addresses.Count(x => x.HyperNetSegmentsWithAbba > 0 && x.SegmentsWithAbba > 0)} addresses with both TLS and Hypernet segments.");
+
+            // Get Part 1 answer - how many inputs with "abba" pattern *outside* [], but *not inside* []?
+            Console.WriteLine
+                ($"\n\nFound {addresses.Count(x => x.SupportsTla())} IPv7 addresses (part 1).");
+            Console.ReadKey();
+        }
+    }
+}
