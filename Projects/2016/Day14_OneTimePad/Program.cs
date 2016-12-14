24a953440409c2405641c1275b9edd80d6f5af09
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-14 23:01:40 +0800

    Start Day 14 Part 2
    Puzzle anser not submitted.

--- Projects/2016/Day14_OneTimePad/Program.cs
+++ Projects/2016/Day14_OneTimePad/Program.cs
@@ -2,11 +2,12 @@
 
 // AdventOfCode: Day14_OneTimePad
 // Created: 2016-12-14
-// Modified: 2016-12-14 10:43 PM
+// Modified: 2016-12-14 11:01 PM
 #endregion
 
 #region Using Directives
 using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -26,36 +27,50 @@ namespace Day14_OneTimePad
         static void Main(string[] args)
         {
             var keyCount = 0;
+            Stopwatch sw = Stopwatch.StartNew();
             while (keyCount != 64)
             {
                 _index++;
 
-                string hash = CalculateMd5Hash($"{Salt}{_index}");
+                string hash = GeneratePart2Hash(CalculateMd5Hash($"{Salt}{_index}"));
                 Match match = KeyRegex.Match(hash);
-                if (match.Success)
+                if (!match.Success) continue;
+
+                // Hash contains a sequence of three characters.
+                // Check that the same character appears in a sequence of 5
+                var keyMatch = new string(match.Value[0], 5);
+                for (var i = 1; i <= 1000; i++)
                 {
-                    // Hash contains a sequence of three characters.
-                    // Check that the same character appears in a sequence of 5
-                    var keyMatch = new string(match.Value[0], 5);
-                    for (var i = 1; i <= 1000; i++)
-                    {
-                        int matchIndex = _index + i;
-                        string nextHash = CalculateMd5Hash($"{Salt}{matchIndex}");
+                    int matchIndex = _index + i;
+                    string nextHash = CalculateMd5Hash($"{Salt}{matchIndex}");
 
-                        // If the hash does not contain the match sequence, continue the for-loop
-                        if (!nextHash.Contains(keyMatch)) continue;
+                    // If the hash does not contain the match sequence, continue the for-loop
+                    if (!nextHash.Contains(keyMatch)) continue;
 
-                        // We found a matching sequence - this is a valid one-time key
-                        keyCount++;
+                    // We found a matching sequence - this is a valid one-time key
+                    keyCount++;
+                    Console.WriteLine($"Found {keyCount} keys...");
 
-                        // Don't bother calculating any remaining hashes - we've already found what we needed.
-                        break;
-                    }
+                    // Don't bother calculating any remaining hashes - we've already found what we needed.
+                    break;
                 }
             }
+            sw.Stop();
+            string msg = $"64th key found at index {_index} in {new TimeSpan(sw.ElapsedTicks):g}.";
 
-            Console.WriteLine($"64th key found at index {_index}");
+            // Just in case I press the any key without catching the result :(
+            Debug.WriteLine(msg);
+            Console.WriteLine(msg);
             Console.ReadKey();
+        }
+
+        private static string GeneratePart2Hash(string hash)
+        {
+            for (var i = 0; i < 2016; i++)
+            {
+                hash = CalculateMd5Hash(hash);
+            }
+            return hash;
         }
 
         /// <summary>
