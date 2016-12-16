d224f4f57ec80bb18a45e90bad710bcc11a15449
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-16 20:51:55 +0800

    Complete Part 1 and 2.

--- Projects/2016/Day15_TimingIsEverything/Program.cs
+++ Projects/2016/Day15_TimingIsEverything/Program.cs
@@ -2,7 +2,7 @@
 
 // AdventOfCode: Day15_TimingIsEverything
 // Created: 2016-12-15
-// Modified: 2016-12-16 7:05 PM
+// Modified: 2016-12-16 8:49 PM
 #endregion
 
 #region Using Directives
@@ -25,38 +25,45 @@ namespace Day15_TimingIsEverything
     {
         #region  Fields
         private static readonly string[] Input = File.ReadAllLines("input.txt");
-        private static readonly List<Disc> Discs = new List<Disc>();
+        private static List<Disc> _discs;
         #endregion
 
         static void Main(string[] args)
         {
             SetupDiscs();
-            int partOneResult = GetTimePartOne();
-            Console.WriteLine($"Press button at {partOneResult} to grab a capsule!");
+            int partOneResult = GetTime();
+            SetupDiscs(true);
+            int partTwoResult = GetTime();
+            Console.WriteLine($"Part 1: Press button at {partOneResult} to grab a capsule!");
+            Console.WriteLine($"Part 2: Press button at {partTwoResult} to grab a capsule!");
             Console.ReadKey();
         }
 
-        private static void SetupDiscs()
+        private static void SetupDiscs(bool part2 = false)
         {
+            _discs = new List<Disc>();
             var regex = new Regex(@"\d+");
             foreach (string s in Input)
             {
                 MatchCollection matches = regex.Matches(s);
-                Discs.Add(new Disc(Convert.ToInt32(matches[0].Value), Convert.ToInt32(matches[1].Value),
-                                   Convert.ToInt32(matches[3].Value)));
+                _discs.Add(new Disc(Convert.ToInt32(matches[1].Value), Convert.ToInt32(matches[3].Value)));
+            }
+            if (part2)
+            {
+                _discs.Add(new Disc(11, 0));
             }
         }
 
-        private static int GetTimePartOne()
+        private static int GetTime()
         {
             // Start timer
             var time = 0;
 
             // Keep going until the ball has fallen through all of the discs.
-            while (!Discs.Last().HasCapsule)
+            while (!_discs.Last().HasCapsule)
             {
                 // Each disc rotates to the next position.
-                foreach (Disc disc in Discs)
+                foreach (Disc disc in _discs)
                 {
                     disc.NextPostion();
                 }
@@ -65,39 +72,56 @@ namespace Day15_TimingIsEverything
                 time++;
 
                 // Capsule falls
-                bool capsulePresent = Discs.Any(x => x.HasCapsule);
+                var capsulePresent = false;
+
+                // Start with first disc
+                var index = 0;
+
+                // While a capsule has not fallen through, and there are still discs to check
+                while (capsulePresent == false && index != _discs.Count - 1)
+                {
+                    //If this disc has a ball, record that fact
+                    if (_discs[index].HasCapsule)
+                    {
+                        capsulePresent = true;
+                    }
+
+                    // Go to the next one
+                    index++;
+                }
 
                 // If a capsule isn't passing through the discs already,
                 // maybe one can begin it's journey.
                 if (!capsulePresent)
                 {
-                    // Is the first disc in the correct position?
-                    Discs[0].HasCapsule = Discs[0].Position == 0;
+                    // If the first disc is at position 0, a capsule has fallen into it.
+                    _discs[0].HasCapsule = _discs[0].Position == 0;
                     continue;
                 }
 
                 // Check each disc.
-                for (var i = 0; i < Discs.Count - 1; i++)
+                for (var i = 0; i < _discs.Count - 1; i++)
                 {
                     // If the disc doesn't have a capsule,
                     // try the next one.
-                    if (!Discs[i].HasCapsule) continue;
+                    if (!_discs[i].HasCapsule) continue;
 
                     // Disc has a capsule.
                     // It drops to the next disc, or bounces away
-                    Discs[i].HasCapsule = false;
-                    if (Discs[i + 1].Position != 0)
+                    _discs[i].HasCapsule = false;
+                    if (_discs[i + 1].Position != 0)
                     {
                         break; // Ball bounced away!
                     }
 
                     // Ball fell through to the next disc.
-                    Discs[i + 1].HasCapsule = true;
+                    _discs[i + 1].HasCapsule = true;
+                    break;
                 }
             }
 
             // Subtract number of discs from timer
-            return time - Discs.Count;
+            return time - _discs.Count;
         }
     }
 }
