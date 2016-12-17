0d640b71ba46a52ea286b2460e9098d09a433dca
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-17 21:14:04 +0800

    Add non-solution for Day 17.

---
+++ Day17_TwoStepsForward/Program.cs
@@ -0,0 +1,25 @@
+﻿#region Information
+
+// AdventOfCode: Day17_TwoStepsForward
+// Created: 2016-12-17
+// Modified: 2016-12-17 9:13 PM
+#endregion
+
+#region Using Directives
+using System;
+
+#endregion
+
+namespace Day17_TwoStepsForward
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var puzzleInput = "pslxynzg";
+            Console.WriteLine("I give up. I don't have the experience to solve this puzzle.");
+            Console.Write("Press any key.");
+            Console.ReadKey();
+        }
+    }
+}
