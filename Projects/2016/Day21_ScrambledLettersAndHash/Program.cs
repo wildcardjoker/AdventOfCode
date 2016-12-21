d10954241c1ae3f75c88be469f20c19019da9a35
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-21 21:09:26 +0800

    Add method logic.

--- Projects/2016/Day21_ScrambledLettersAndHash/Program.cs
+++ Projects/2016/Day21_ScrambledLettersAndHash/Program.cs
@@ -2,7 +2,7 @@
 
 // AdventOfCode: Day21_ScrambledLettersAndHash
 // Created: 2016-12-21
-// Modified: 2016-12-21 8:45 PM
+// Modified: 2016-12-21 9:09 PM
 #endregion
 
 #region Using Directives
@@ -39,10 +39,45 @@ namespace Day21_ScrambledLettersAndHash
             Input[indexB] = temp;
         }
 
-        static void RotateLeft(int steps) {}
-        static void RotateRight(int steps) {}
-        static void RotateOnPosition(char c) {}
-        static void ReversePositionsXThroughY(int x, int y) {}
-        static void MovePosXToPosY(int x, int y) {}
+        static void RotateLeft(int steps)
+        {
+            for (var i = 0; i < steps; i++)
+            {
+                char temp = Input[0];
+                Input.RemoveAt(0);
+                Input.Add(temp);
+            }
+        }
+
+        static void RotateRight(int steps)
+        {
+            int index = Input.Count - 1;
+            for (var i = 0; i < steps; i++)
+            {
+                char temp = Input[index];
+                Input.RemoveAt(index);
+                Input.Insert(0, temp);
+            }
+        }
+
+        static void RotateOnPosition(char c)
+        {
+            int index = Input.IndexOf(c);
+
+            // Rotate index+1, +1 again if index>= 4
+            RotateRight(index + index >= 4 ? 2 : 1);
+        }
+
+        static void ReversePositionsXThroughY(int x, int y)
+        {
+            Input.Reverse(x, y);
+        }
+
+        static void MovePosXToPosY(int x, int y)
+        {
+            char temp = Input[x];
+            Input.RemoveAt(x);
+            Input.Insert(y, temp);
+        }
     }
 }
