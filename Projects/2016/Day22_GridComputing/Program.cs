9cf9c27a49eac4a967944c09b05e17ea69306b82
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-23 22:04:09 +0800

    Completed Day 22 Part 2 (with help from /u/AndrewGreenH)

--- Projects/2016/Day22_GridComputing/Program.cs
+++ Projects/2016/Day22_GridComputing/Program.cs
@@ -2,7 +2,7 @@
 
 // AdventOfCode: Day22_GridComputing
 // Created: 2016-12-23
-// Modified: 2016-12-23 9:42 PM
+// Modified: 2016-12-23 10:03 PM
 #endregion
 
 #region Using Directives
@@ -24,14 +24,18 @@ namespace Day22_GridComputing
 
         static void Main(string[] args)
         {
-            var nodeCount = 0;
-            foreach (Node node in Nodes.Where(x => x.Used != 0))
-            {
-                nodeCount += Nodes.Count(x => !(x.PosX == node.PosX && x.PosY == node.PosY) && x.Available >= node.Used);
-            }
+            int nodeCount =
+                Nodes.Where(x => x.Used != 0)
+                     .Sum(
+                         node =>
+                             Nodes.Count(
+                                 x => !(x.PosX == node.PosX && x.PosY == node.PosY) && x.Available >= node.Used));
             Console.WriteLine($"{Nodes.Count} nodes found.");
             Console.WriteLine($"{Nodes.Count(x => x.Used != 0)} nodes are used.");
             Console.WriteLine($"{nodeCount} node pairs found.");
+            Console.Write("Part 2 solved using http://codepen.io/anon/pen/mOYdKJ");
+            Console.WriteLine(
+                "Part 2 code created by /u/AndrewGreenH at https://www.reddit.com/r/adventofcode/comments/5jor9q/2016_day_22_solutions/dbi1999/");
             Console.ReadKey();
         }
     }
