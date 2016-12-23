4b1babb07f0f74c35c8c347a83b8ecf09b594a49
wildcardjoker <the.pctechnician+github@gmail.com>
2016-12-23 21:19:32 +0800

    Begin Day 22.

---
+++ Projects/2016/Day22_GridComputing/Program.cs
@@ -0,0 +1,25 @@
+﻿#region Information
+
+// AdventOfCode: Day22_GridComputing
+// Created: 2016-12-23
+// Modified: 2016-12-23 9:18 PM
+#endregion
+
+#region Using Directives
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+#endregion
+
+namespace Day22_GridComputing
+{
+    class Program
+    {
+        #region  Fields
+        private static readonly List<Node> Nodes = File.ReadAllLines("input.txt").Select(x => new Node(x)).ToList();
+        #endregion
+
+        static void Main(string[] args) {}
+    }
+}
