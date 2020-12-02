9d4c437e43eb22fe0e88bdff73f8aa3ab173f07a
MOORE Jason <JasonMo@intranet.frederickirwin.wa.edu.au>
2020-12-02 13:18:35 +0800

    Completed Day 1 Part 1.

---
+++ 2020/2020Day1/Program.cs
@@ -0,0 +1,48 @@
+﻿// 2020Day1
+
+namespace _2020Day1
+{
+    #region Using Directives
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    #endregion
+
+    internal class Program
+    {
+        #region Fields
+        private static List<int> _input = new List<int>();
+        #endregion
+
+        private static void Main(string[] args)
+        {
+            // Sample input
+            // _input = new[] {1721, 979, 366, 299, 675, 1456}.ToList();
+            _input = File.ReadAllLines("input.txt").Select(num => Convert.ToInt32(num)).ToList();
+
+            Part1();
+        }
+
+        private static void Part1()
+        {
+            var sumTarget = 2020;
+            var i         = 0;
+            var num1      = 0;
+            var num2      = 0;
+            while (num1 + num2 != sumTarget)
+            {
+                num1 = _input[i];
+                var targetNum = sumTarget - num1;
+                if (_input.Contains(targetNum))
+                {
+                    num2 = targetNum;
+                }
+
+                i++;
+            }
+
+            Console.WriteLine($"Found entries {num1} and {num2} in {i} tries. Day 1 Part 1 Result: {num1 * num2}");
+        }
+    }
+}
---
+++ 2020/readme.md
@@ -0,0 +1,5 @@
+# Hello .NET Core 3!
+
+I won't be completing this year's challenge - I have too much happening at this time of year unfortunately.
+
+However, I will enjoy solving as many puzzles as I can, and learning the ins and outs of .NET Core 3 and .NET 5 while I do so.
