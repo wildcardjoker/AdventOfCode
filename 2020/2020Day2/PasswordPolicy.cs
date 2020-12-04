8c99bbebe3bf2622b0647279fa91d6296ce2f1a0
MOORE Jason <JasonMo@intranet.frederickirwin.wa.edu.au>
2020-12-04 13:22:42 +0800

    Add 2020 Day 2 part 1.

---
+++ 2020/2020Day2/PasswordPolicy.cs
@@ -0,0 +1,44 @@
+﻿// 2020Day2
+
+namespace _2020Day2
+{
+    #region Using Directives
+    using System;
+    using System.Linq;
+    #endregion
+
+    public class PasswordPolicy
+    {
+        #region Constructors
+        public PasswordPolicy(string input)
+        {
+            // Extract min/max
+            var minMax = input.Substring(0, input.IndexOf(' ') + 1).Split('-');
+            MinValue = Convert.ToInt32(minMax[0]);
+            MaxValue = Convert.ToInt32(minMax[1]);
+            Letter   = input[input.IndexOf(' ') + 1];
+            Password = input.Substring(input.LastIndexOf(' '));
+        }
+
+        #endregion
+
+        #region Properties
+        public bool IsValid
+        {
+            get
+            {
+                var charCount = Password.Count(x => x.Equals(Letter));
+                return charCount >= MinValue && charCount <= MaxValue;
+            }
+        }
+
+        public char Letter {get; set;}
+
+        public int MaxValue {get; set;}
+
+        public int MinValue {get; set;}
+
+        public string Password {get; set;}
+        #endregion
+    }
+}
---
+++ 2020/2020Day2/Program.cs
@@ -0,0 +1,31 @@
+﻿// 2020Day2
+
+namespace _2020Day2
+{
+    #region Using Directives
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    #endregion
+
+    internal class Program
+    {
+        #region Fields
+        private static List<PasswordPolicy> _input;
+        #endregion
+
+        private static void Main(string[] args)
+        {
+            var inputFilename = "Input.txt"; // "Sample.txt";
+            _input = File.ReadAllLines(inputFilename).Select(x => new PasswordPolicy(x)).ToList();
+            PartOne();
+        }
+
+        private static void PartOne()
+        {
+            var validPasswords = _input.Count(x => x.IsValid);
+            Console.WriteLine($"Part One: Found {validPasswords} valid passwords.");
+        }
+    }
+}
