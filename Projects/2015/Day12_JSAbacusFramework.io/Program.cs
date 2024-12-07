f0fa84c9864a3c5ad3dd3e6f25c262b797666116
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-12-07 21:10:20 +0800

    2015 Day 12 Part 2
    Parse JSON and exclude all `red` properties. Modified from https://www.markheath.net/post/advent-of-code-day12

--- Projects/2015/Day12_JSAbacusFramework.io/Program.cs
+++ Projects/2015/Day12_JSAbacusFramework.io/Program.cs
@@ -1,49 +1,58 @@
 ﻿#region Information
-
 // AdventOfCode: Day12_JSAbacusFramework.io
 // Created: 2015-12-12
 // Modified: 2015-12-12 9:53 PM
 // Last modified by: Jason Moore (Jason)
 #endregion
 
-#region Using Directives
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-#endregion
-
 namespace Day12_JSAbacusFramework.io
 {
-    class Program
+    #region Using Directives
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using Newtonsoft.Json.Linq;
+    #endregion
+
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            string input = File.ReadAllText("input.txt");
-            string part1Pattern = "-?\\d+";
-            string part2Pattern = @"({^\[.*red.*^\]})|(.)";
-
-            // Part 1
-            GetSum(input, part1Pattern);
-
-            // Part 2
-            Regex regex = new Regex(part2Pattern);
-            input = Regex.Replace(input, part2Pattern, "$2");
-            GetSum(input, part1Pattern);
-            Console.Write("Press any key.");
-            Console.ReadKey();
-        }
-
         private static void GetSum(string input, string pattern)
         {
-            Regex regex = new Regex(pattern);
-            int i = regex.Matches(input).Cast<Match>().Sum(match => Convert.ToInt32(match.Value));
+            var regex = new Regex(pattern);
+            var i     = regex.Matches(input).Cast<Match>().Sum(match => Convert.ToInt32(match.Value));
 
-            string result = $"Sum Total of all values: {i}.";
+            var result = $"Sum Total of all values (part 1): {i}.";
             Console.WriteLine(result);
-            Debug.WriteLine(result);
+        }
+
+        private static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt");
+
+            // Part 1
+            GetSum(input, "-?\\d+");
+
+            // Part 2
+            var json = JArray.Parse(input);
+            Console.WriteLine($"Sum Total of all values (part 2): {SumTokens(json)}.");
+        }
+
+        private static long SumTokens(JToken token)
+        {
+            switch (token)
+            {
+                case JObject jo:
+                    return jo.Properties().Select(j => j.Value).OfType<JValue>().Select(k => k.Value).OfType<string>().Any(propValue => propValue.Equals("red"))
+                               ? 0
+                               : jo.Properties().Select(p => p.Value).Sum(SumTokens);
+                case JArray ja:
+                    return ja.Sum(SumTokens);
+                case JValue jv:
+                    return jv.Value is long value ? value : 0;
+                default:
+                    throw new InvalidOperationException();
+            }
         }
     }
 }
